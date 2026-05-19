f783226ed86b6e31a1beb589862bf346aafd0893
wi_fi <sonax.x1416@gmail.com>
2026-05-19 15:41:42 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='wi_fi',
+    version='0.1',
+    author='Yazeed-Bader',
+    description='A library in Python specialized in electronic networks',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: Windows',
+        'License :: OSI Approved :: MIT License'
+    ]
+)

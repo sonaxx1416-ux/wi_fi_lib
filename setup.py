4f2c894d199f09811abf14bfacaa51f8fbfcb510
wi_fi <sonax.x1416@gmail.com>
2026-05-21 14:27:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,1 @@
-import setuptools
 
-setuptools.setup(
-    name='wi_fi',
-    version='0.1',
-    author='Yazeed-Bader',
-    description='A library in Python specialized in electronic networks',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: Windows',
-        'License :: OSI Approved :: MIT License'
-    ]
-)
